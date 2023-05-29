2b79e34f20800ad5ebf74eb37537007a612cd599
SheesHaseem <134010458+SheesHaseem@users.noreply.github.com>
2023-05-29 19:42:49 -0400

    Time: 6122 ms (5.01%), Space: 19.9 MB (46.28%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,13 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs):
         strs2 = []
-        x = -1
         s = len(strs)
         for i in range(s):
             strs2.append(''.join(sorted(strs[i])))
         strsset = set(strs2)
         ss = len(strsset)
-        lists = [[] for j in range(ss)]
+        lists = [[] for i in range(ss)]
         strs3 = list(strsset)
         for i in range(s):
             for j in range(ss):
